6af74a991e6f94c33fe4b5e5fe975871444a3bbb
Gustavo Lima <gustavo.lima@nd.com.br>
2023-11-08 16:53:32 -0300

    feat

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+  return transaction
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('server running')
+  })
